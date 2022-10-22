e86dfa9fb6c43b1935b6b4b0dc3e645225fdfa72
Hans Reier Sigmond <reier.sigmond@gmail.com>
2022-10-22 19:25:02 +0200

    Fått Kotlin Hibernate test til å funke

--- eDrops/src/test/kotlin/com/sigmondsmart/edrops/domain/TestBlogEntry.kt
+++ eDrops/src/test/kotlin/com/sigmondsmart/edrops/domain/TestBlogEntry.kt
@@ -1,54 +1,68 @@
 package com.sigmondsmart.edrops.domain
 
-//import org.hibernate.SessionFactory
-//import org.hibernate.testing.orm.junit.SessionFactory
-//import org.junit.jupiter.api.Assertions.assertTrue
-//import org.junit.jupiter.api.Test
 import edrops.domain.BlogEntry
-import org.hibernate.Session
-import org.hibernate.testing.orm.junit.SessionFactory
+import org.hibernate.cfg.Configuration
 import org.hibernate.testing.transaction.TransactionUtil.doInHibernate
 import org.junit.Assert.assertTrue
+import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase
 import org.junit.Test
+import java.io.IOException
+
 import java.time.LocalDateTime
+import java.util.*
 
 // TODO Why not possible, pom problem
 //https://www.baeldung.com/kotlin/jpa
-//Problem: Uses JUNIT 4.
-class TestBlogEntry {
+//Note must use JUNIT 4 due to inheritance from BaseCoreFunctionalTestCase (Hibernate 5).
+// Hibernate 6 connot be used in Spring yet (wait for Spring 3.0).
+class TestBlogEntry : BaseCoreFunctionalTestCase() {
 
-   // val sessionFactory: SessionFactory = TODO()
+    private val properties: Properties
+        @Throws(IOException::class)
+        get() {
+            val properties = Properties()
+            properties.load(javaClass.classLoader.getResourceAsStream("hibernate.properties"))
+            return properties
+        }
+
+    override fun getAnnotatedClasses(): Array<Class<*>> {
+        return arrayOf(BlogEntry::class.java)
+    }
+
+    override fun configure(configuration: Configuration) {
+        super.configure(configuration)
+        configuration.properties = properties
+    }
 
     @Test
     fun givenBlogEntry_whenSaved_thenFound() {
-        doInHibernate<Unit>({ this.sessionFactory() },
-            { session ->
+        doInHibernate({ sessionFactory() }) { session ->
             val blogEntry = BlogEntry(
-                LocalDateTime.now(), null,"Dette er Reiers første blog")
+                LocalDateTime.now(), null, "Dette er Reiers første blog"
+            )
             session.persist(blogEntry)
             val blogEntryFound = session.find(BlogEntry::class.java, blogEntry.id)
             session.refresh(blogEntryFound)
 
             assertTrue(blogEntry.text == blogEntryFound.text)
-        })
-
-    }
-
-    private fun sessionFactory() {
-        TODO("Not yet implemented")
+        }
     }
 
     @Test
-    fun createValidUser_saveUser_userSaved() {
-        doInHibernate<SessionFactory>(({this.sessionFactory()) }), {session: Session ->
+    fun givenAddressWithDefaultEquals_whenAddedToSet_thenNotFound() {
+        doInHibernate({ sessionFactory() }) { session ->
             val blogEntry = BlogEntry(
                 LocalDateTime.now(), null,"Dette er Reiers første blog")
             session.persist(blogEntry)
+            val blogEntryFound = session.find(BlogEntry::class.java, blogEntry.id)
+            session.refresh(blogEntryFound)
+            assertTrue(blogEntry.text == blogEntryFound.text)
+        }
 
-            val blogEntryFound  = session.find(BlogEntry::class.java, blogEntry.id)
 
-          //  assertThat(actualUser).isEqualTo(expectedUser)
-        })
     }
 
+    //private fun sessionFactory() {
+    //    TODO("Not yet implemented")
+    //}
 }
