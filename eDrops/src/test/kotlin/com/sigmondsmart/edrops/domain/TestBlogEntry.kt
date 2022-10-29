876d1d19f37b5e51bf8764a127d9fa3fa07f890e
Hans Reier Sigmond <reier.sigmond@gmail.com>
2022-10-29 19:16:03 +0200

    simplified code. Name of config file must be hibernate.cfg anyhow.

--- eDrops/src/test/kotlin/com/sigmondsmart/edrops/domain/TestBlogEntry.kt
+++ eDrops/src/test/kotlin/com/sigmondsmart/edrops/domain/TestBlogEntry.kt
@@ -1,36 +1,20 @@
 package com.sigmondsmart.edrops.domain
 
 import edrops.domain.BlogEntry
-import org.hibernate.cfg.Configuration
+import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase
 import org.hibernate.testing.transaction.TransactionUtil.doInHibernate
 import org.junit.Assert.assertTrue
-import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase
 import org.junit.Test
-import java.io.IOException
-
 import java.time.LocalDateTime
-import java.util.*
 
 // https://www.baeldung.com/kotlin/jpa
 // Note must use JUNIT 4 due to inheritance from BaseCoreFunctionalTestCase (Hibernate 5).
+// Config file must be called hibernate.cfg
 // Hibernate 6 connot be used in Spring yet (wait for Spring 3.0).
+// Actually want to get rid of the code, due to these weaknesses.
 class TestBlogEntry : BaseCoreFunctionalTestCase() {
-
-    private val properties: Properties
-        @Throws(IOException::class)
-        get() {
-            val properties = Properties()
-            properties.load(javaClass.classLoader.getResourceAsStream("hibernate.properties"))
-            return properties
-        }
-
     override fun getAnnotatedClasses(): Array<Class<*>> {
         return arrayOf(BlogEntry::class.java)
-    }
-
-    override fun configure(configuration: Configuration) {
-        super.configure(configuration)
-        configuration.properties = properties
     }
 
     @Test
@@ -52,7 +36,8 @@ class TestBlogEntry : BaseCoreFunctionalTestCase() {
     fun givenAddressWithDefaultEquals_whenAddedToSet_thenNotFound() {
         doInHibernate({ sessionFactory() }) { session ->
             val blogEntry = BlogEntry(
-                LocalDateTime.now(), null,"Dette er Reiers første blog")
+                LocalDateTime.now(), null, "Dette er Reiers første blog"
+            )
             log.info("testing Hibernate persistence 2")
             session.persist(blogEntry)
             val blogEntryFound = session.find(BlogEntry::class.java, blogEntry.id)
