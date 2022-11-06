fd9b6cb6a52328693c624e4401a5ebe911f0d605
Hans Reier Sigmond <reier.sigmond@gmail.com>
2022-11-06 17:48:01 +0100

    Innført @DataJpaTest

--- eDrops/src/test/kotlin/com/sigmondsmart/edrops/repository/TestBlogEntry.kt
+++ eDrops/src/test/kotlin/com/sigmondsmart/edrops/repository/TestBlogEntry.kt
@@ -6,12 +6,12 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import java.time.LocalDateTime
 
 @ExtendWith(SpringExtension::class)
-@SpringBootTest
+@DataJpaTest
 class TestBlogEntry(@Autowired val repo: BlogEntryRepository) {
 
     @Test
