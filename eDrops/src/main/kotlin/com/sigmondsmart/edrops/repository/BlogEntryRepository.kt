f390c2e70eeb8c62764d98099cccc2c202910134
Hans Reier Sigmond <reier.sigmond@gmail.com>
2022-11-06 17:32:37 +0100

    Sjekket inn filer som manglet

---
+++ eDrops/src/main/kotlin/com/sigmondsmart/edrops/repository/BlogEntryRepository.kt
@@ -0,0 +1,11 @@
+package com.sigmondsmart.edrops.repository
+
+import com.sigmondsmart.edrops.domain.BlogEntry
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface BlogEntryRepository : JpaRepository<BlogEntry, Long> {
+  //  fun getBlogEntry(id: Long): BlogEntry?
+  fun findByText(text: String): Iterable<BlogEntry>
+}
