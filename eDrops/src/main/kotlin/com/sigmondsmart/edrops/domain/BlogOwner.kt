9f282a50a6c54b593caca256b9c541ac0844c851
Hans Reier Sigmond <reier.sigmond@gmail.com>
2022-11-11 18:54:33 +0100

    Lagt inn ny entitet

---
+++ eDrops/src/main/kotlin/com/sigmondsmart/edrops/domain/BlogOwner.kt
@@ -0,0 +1,52 @@
+package com.sigmondsmart.edrops.domain
+
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity
+class BlogOwner(
+
+    @Column(nullable = false)
+    val created: LocalDateTime,
+
+    @Column
+    var changed: LocalDateTime?,
+
+    @Column
+    var firstName: String,
+
+    @Column
+    var lastName: String,
+
+    @Column
+    var eMail: String,
+
+    @Column
+    var phones: String,
+
+    @Column
+    var address: String,
+
+    @Column
+    var zipCode: String,
+
+    @Column
+    var location: String,
+
+    @Column
+    var county: String,
+
+    @Column
+    var country: String,
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @Column(nullable = true)
+    @OneToMany(cascade = [CascadeType.ALL])
+    val blogEntries: List<BlogEntry>? = null
+
+) {
+    override fun toString() = "id: $id created: $created changed: $changed name: $firstName $lastName"
+}
