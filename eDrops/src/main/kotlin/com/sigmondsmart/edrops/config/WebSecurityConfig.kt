9568d3e598ede5d1791a72a373ccdd62538e7328
Hans Reier Sigmond <reier.sigmond@gmail.com>
2022-11-29 08:34:32 +0100

    Flyttet konfig over til Kotlin

---
+++ eDrops/src/main/kotlin/com/sigmondsmart/edrops/config/WebSecurityConfig.kt
@@ -0,0 +1,26 @@
+package com.sigmondsmart.edrops.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+
+@Configuration
+@EnableWebSecurity
+class WebSecurityConfig {
+
+    @Bean
+    @Throws(Exception::class)
+    fun filterChain(http: HttpSecurity): SecurityFilterChain? {
+        http
+            .httpBasic().disable()
+            .authorizeRequests()
+            .antMatchers("/*").permitAll()
+            .antMatchers("/h2-console/**").permitAll()
+            .anyRequest().authenticated()
+        http.csrf().disable().headers().frameOptions().disable() //needed to enable H2 console
+        return http.build()
+    }
+}
