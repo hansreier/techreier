98c1bbe893fe792af4f281a2cc2f97872497cbf2
Hans Reier Sigmond <reier.sigmond@gmail.com>
2023-01-21 14:29:39 +0100

    flyttet blog teksten til egen entitet

---
+++ eDrops/src/main/kotlin/com/sigmondsmart/edrops/config/Init.kt
@@ -0,0 +1,17 @@
+package com.sigmondsmart.edrops.config
+
+import com.sigmondsmart.edrops.domain.BlogData
+import com.sigmondsmart.edrops.repository.BlogOwnerRepository
+import com.sigmondsmart.edrops.repository.LanguageRepository
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class Init(languageRepo: LanguageRepository, ownerRepo: BlogOwnerRepository) {
+    init {
+        logger.info("Reier was here mama")
+        val blogData = BlogData()
+        languageRepo.save(blogData.norwegian)
+        ownerRepo.save(blogData.blogOwner)
+        logger.info("Initialized with data")
+    }
+}
