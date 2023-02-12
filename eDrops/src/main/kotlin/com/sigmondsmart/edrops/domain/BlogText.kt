08c4392577c2011fa11ce1375850aab54449fdc0
Hans Reier Sigmond <reier.sigmond@gmail.com>
2023-02-12 18:25:22 +0100

    endring i kommentar

--- eDrops/src/main/kotlin/com/sigmondsmart/edrops/domain/BlogText.kt
+++ eDrops/src/main/kotlin/com/sigmondsmart/edrops/domain/BlogText.kt
@@ -8,7 +8,9 @@ class BlogText(
     @Column
     var text: String,
 
-    //Note: Bidirectional relationship one to one creates trouble with eager fetching
+    // Note: Bidirectional relationship one to one creates trouble with eager fetching
+    // https://stackoverflow.com/questions/1444227/how-can-i-make-a-jpa-onetoone-relation-lazy
+    // And not mandatory BlogTexts create other problems...
     @OneToOne(fetch = FetchType.LAZY)
     @MapsId
     var blogEntry: BlogEntry,
