c59979de2af44f317a744e49885c75a8d11929d3
Hans Reier Sigmond <reier.sigmond@gmail.com>
2023-04-30 23:51:23 +0200

    fikset tabell extension

--- eDrops/src/main/kotlin/com/sigmondsmart/edrops/util/Markdown.kt
+++ eDrops/src/main/kotlin/com/sigmondsmart/edrops/util/Markdown.kt
@@ -1,19 +1,23 @@
 package com.sigmondsmart.edrops.util
 
+import org.commonmark.Extension
+import org.commonmark.ext.gfm.tables.TablesExtension
 import org.commonmark.parser.Parser
 import org.commonmark.renderer.html.HtmlRenderer
 import org.slf4j.LoggerFactory
 import java.io.File
 import java.io.FileNotFoundException
+import java.util.*
+
 
 fun markdownToHtml(file: String): String {
     val logger = LoggerFactory.getLogger("markdownToHtml")
     try {
         val readme = File(file).readText(Charsets.UTF_8)
-        val parser: Parser = Parser.builder()
-            .build()
+        val exts: List<Extension> = Arrays.asList(TablesExtension.create())
+        val parser: Parser = Parser.builder().extensions(exts).build()
         val document = parser.parse(readme)
-        val renderer = HtmlRenderer.builder().build()
+        val renderer = HtmlRenderer.builder().extensions(exts).build()
         return renderer.render(document)
     } catch (e: FileNotFoundException) {
         logger.error("Not found: $file")
