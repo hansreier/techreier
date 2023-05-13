7394f9ef7d2411f769761b9ee3ce638334a2bb83
Hans Reier Sigmond <reier.sigmond@gmail.com>
2023-05-13 16:48:44 +0200

    Forsøker å endre layout på bilder

---
+++ eDrops/src/main/kotlin/com/sigmondsmart/edrops/util/ImageAttributeProvider.kt
@@ -0,0 +1,13 @@
+package com.sigmondsmart.edrops.util
+
+import org.commonmark.node.Image
+import org.commonmark.node.Node
+import org.commonmark.renderer.html.AttributeProvider
+// https://github.com/commonmark/commonmark-java
+class ImageAttributeProvider : AttributeProvider {
+    override fun setAttributes(node: Node?, tagName: String?, attributes: MutableMap<String?, String?>) {
+        if (node is Image) {
+            attributes["width"] = "300"
+        }
+    }
+}
