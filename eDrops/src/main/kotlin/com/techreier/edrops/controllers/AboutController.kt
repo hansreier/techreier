c7d30e02461aee0bca35391b60e021def3833250
Hans Reier Sigmond <reier.sigmond@gmail.com>
2023-10-05 19:13:05 +0200

    Lagt inn start på node visitor

--- eDrops/src/main/kotlin/com/techreier/edrops/controllers/AboutController.kt
+++ eDrops/src/main/kotlin/com/techreier/edrops/controllers/AboutController.kt
@@ -11,12 +11,13 @@ import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.mvc.support.RedirectAttributes
 
 @Controller
-@RequestMapping("/about")
+@RequestMapping("/about/{id}")
 class AboutController(dbService: DbService) : BaseController(dbService) {
     @GetMapping
-    fun content(request: HttpServletRequest, model: Model): String {
+    fun content( @PathVariable id: Long, request: HttpServletRequest, model: Model ): String {
         logger.info("content: ${request.servletPath}")
         val blogParams = setCommonModelParameters(model, request)
+        logger.info("reierid: $id")
         val doc =  Docs.getDoc(blogParams.blogId)
         val docText: String = markdownToHtml(doc)
         model.addAttribute("docText", docText)
