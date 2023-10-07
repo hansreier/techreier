7bcfc20ba56b16c634ad1838fc70807c624ed71f
Hans Reier Sigmond <reier.sigmond@gmail.com>
2023-10-07 11:16:46 +0200

    litt kodeforenkling

--- eDrops/src/main/kotlin/com/techreier/edrops/util/Doc.kt
+++ eDrops/src/main/kotlin/com/techreier/edrops/util/Doc.kt
@@ -3,14 +3,4 @@ package com.techreier.edrops.util
 import com.techreier.edrops.domain.LanguageCode
 
 data class Doc(val tag: String, val language: LanguageCode, val subject: String? = null,
-               val ext: Boolean = true) {
-    private companion object {
-        var count = 0L
-    }
-
-    val id: Long = count
-
-    init {
-        count++
-    }
-}
+               val ext: Boolean = true)
