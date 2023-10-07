4880e5cdd1af5f8ab337f77ad6ab21a086e13f5a
Hans Reier Sigmond <reier.sigmond@gmail.com>
2023-10-07 09:27:00 +0200

    Litt renaming

--- eDrops/src/main/kotlin/com/techreier/edrops/controllers/AboutController.kt
+++ eDrops/src/main/kotlin/com/techreier/edrops/controllers/AboutController.kt
@@ -16,10 +16,9 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes
 class AboutController(dbService: DbService) : BaseController(dbService) {
     @GetMapping
     fun content(@PathVariable(required = false) tag: String?, request: HttpServletRequest, model: Model): String {
-        logger.info("content: ${request.servletPath}")
         val blogParams = setCommonModelParameters(model, request)
         val docIndex: Long = getDocIndex(tag, blogParams.langCode).toLong()
-        logger.info("Tag: ${tag} BlogId: ${blogParams.blogId} DocIndex: $docIndex Language: ${blogParams.langCode} ")
+        logger.debug("Tag: ${tag} BlogId: ${blogParams.blogId} DocIndex: $docIndex Language: ${blogParams.langCode} ")
         val doc = Docs.getDoc(if (docIndex == 0L) blogParams.blogId else docIndex)
         val docText: String = markdownToHtml(doc)
         model.addAttribute("docText", docText)
