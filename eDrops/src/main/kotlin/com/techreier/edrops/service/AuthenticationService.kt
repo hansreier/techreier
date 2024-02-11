2bea4ef82e139184340fe3eb48cab26f9216710d
Hans Reier Sigmond <reier.sigmond@gmail.com>
2024-02-11 22:29:45 +0100

    legger opp til at brukernavn og passord lagres på blogowner i databasen.

--- eDrops/src/main/kotlin/com/techreier/edrops/service/AuthenticationService.kt
+++ eDrops/src/main/kotlin/com/techreier/edrops/service/AuthenticationService.kt
@@ -2,20 +2,38 @@ package com.techreier.edrops.service
 
 import com.techreier.edrops.config.logger
 import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.authentication.BadCredentialsException
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
 
+
+@Service
 class AuthenticationService(
     private val userDetailsService: UserDetailsService,
-    private val passwordEncoder: BCryptPasswordEncoder
+    private val passwordEncoder: PasswordEncoder
 ): AuthenticationProvider {
 
-    override fun authenticate(authentication: Authentication?): Authentication {
-        logger.info("Authenticationg")
-        TODO("Not yet implemented")
+    override fun authenticate(authentication: Authentication): Authentication {
+        logger.info("Authenticating")
+        val name = authentication.name
+        val password = authentication.credentials.toString()
+        val userDetails = userDetailsService.loadUserByUsername(name)
+        return checkPassword(userDetails, password, passwordEncoder)
     }
 
+    // TODO Always true OK?
     override fun supports(authentication: Class<*>?): Boolean {
-        TODO("Not yet implemented")
+        return true
+    }
+
+    private fun checkPassword(userDetails: UserDetails, rawPassword: String, encoder: PasswordEncoder): Authentication {
+        if (encoder.matches(rawPassword, userDetails.password)) return UsernamePasswordAuthenticationToken(
+            userDetails.username, userDetails.password, userDetails.authorities
+        ) else {
+            throw BadCredentialsException("Bad credentials")
+        }
     }
 }
