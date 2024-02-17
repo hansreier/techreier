975335a35c633991d38988c299074773649a597c
Hans Reier Sigmond <reier.sigmond@gmail.com>
2024-02-17 11:39:58 +0100

    Fjernet unødvendig tidligere konfig og testklasser. Forbedret konfig test.

--- eDrops/src/test/kotlin/com/techreier/edrops/TestConfig.kt
+++ eDrops/src/test/kotlin/com/techreier/edrops/TestConfig.kt
@@ -1,36 +1,29 @@
 package com.techreier.edrops
 
 import com.techreier.edrops.config.AppConfig
-import com.techreier.edrops.config.ReadConfig
 import com.techreier.edrops.config.logger
-import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest
 class TestConfig {
+
     @Autowired
     private lateinit var appConfig: AppConfig
 
-    @Autowired
-    private lateinit var readConfig: ReadConfig
-
-    @BeforeEach
-    fun setUp() {
-        readConfig =  ReadConfig(config = appConfig)
-    }
+    @Value("\${app.appname}")
+    private val appname: String = ""
 
     @Test
-    fun testAppNameIndirect() {
-        logger.info("App name: ${readConfig.getConfig()}")
-    }
-
-    @Test
-    fun testAppNameDirect() {
+    fun testAppConfig() {
         logger.info("App name: ${appConfig.appname}")
+        Assertions.assertEquals(appname, appConfig.appname)
+        Assertions.assertNotNull(appConfig.password)
     }
 }
