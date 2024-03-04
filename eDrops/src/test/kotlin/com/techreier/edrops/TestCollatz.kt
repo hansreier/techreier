4ee9ec73b2370bdcf0fab61aa2f508855e89b00c
Hans Reier Sigmond <reier.sigmond@gmail.com>
2024-03-04 22:25:42 +0100

    lagt til tulletest

--- eDrops/src/test/kotlin/com/techreier/edrops/TestCollatz.kt
+++ eDrops/src/test/kotlin/com/techreier/edrops/TestCollatz.kt
@@ -1,19 +1,13 @@
 package com.techreier.edrops
 
 import com.techreier.edrops.config.logger
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.fail
 
-const val MAX_ITERATIONS = 500
-const val MIN_SIZE = 10
-const val MAX_SIZE = 100
+const val MIN_SIZE = 1
+const val MAX_SIZE = 500000
 
 class TestCollatz {
-    @BeforeEach
-    fun setUp() {
-        logger.info("BeforeEach")
-    }
+
     @Test
     fun testCollatz() {
         for (start in MIN_SIZE..MAX_SIZE) {
@@ -26,11 +20,7 @@ class TestCollatz {
                 } else {
                     value = value * 3 + 1
                 }
-        } while ((value != 1) and (iterations <= MAX_ITERATIONS) )
-            if (iterations > MAX_ITERATIONS) {
-                logger.info("$MAX_ITERATIONS iterations exceeded, stopping")
-                fail("not one")
-            }
+        } while ((value != 1))
             logger.info("seed $start is 1 after $iterations iterations")
         }
     }
