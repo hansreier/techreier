fbad46e619db2d5877bd841154ed0880dde37bb6
Hans Reier Sigmond <reier.sigmond@gmail.com>
2024-03-05 18:46:35 +0100

    forbedret output

--- eDrops/src/test/kotlin/com/techreier/edrops/TestCollatz.kt
+++ eDrops/src/test/kotlin/com/techreier/edrops/TestCollatz.kt
@@ -4,13 +4,18 @@ import com.techreier.edrops.config.logger
 import org.junit.jupiter.api.Test
 
 const val MIN_SIZE = 1L
-const val MAX_SIZE = 1000000L
+const val MAX_SIZE = 30L
 
+//Collatz Conjecture
+//Collatx formodning
+//https://www.quantamagazine.org/why-mathematicians-still-cant-solve-the-collatz-conjecture-20200922/
 class TestCollatz {
 
     @Test
     fun testCollatz() {
+         val displaySequence = true
         for (start in MIN_SIZE..MAX_SIZE) {
+            val sequence = StringBuilder()
             var value = start
             var iterations = 0
             do {
@@ -20,8 +25,12 @@ class TestCollatz {
                 } else {
                     value = value * 3 + 1
                 }
+          if (displaySequence) {
+              sequence.append('→')
+              sequence.append(value)
+          }
         } while ((value != 1L))
-            logger.info("seed $start is 1 after $iterations iterations")
+            logger.info("$start X $iterations ${sequence}")
         }
     }
 }
