eea1239ff7f1c2637f8d056069ecd7cec50fe027
Hans Reier Sigmond <reier.sigmond@gmail.com>
2024-03-05 19:07:45 +0100

    forbedret output

--- eDrops/src/test/kotlin/com/techreier/edrops/TestCollatz.kt
+++ eDrops/src/test/kotlin/com/techreier/edrops/TestCollatz.kt
@@ -7,13 +7,16 @@ const val MAX_SIZE = 30L
 const val DISPLAY_SEQUENCE = true
 
 //Collatz Conjecture
-//Collatx formodning
+//Collatz formodning
 //https://www.quantamagazine.org/why-mathematicians-still-cant-solve-the-collatz-conjecture-20200922/
+//divide by two if result is integer else multiply by 3 and add one. Repeat.
+//You're out if the number is 1 or more than 100
 class TestCollatz {
 
     @Test
     fun testCollatz() {
         for (start in MIN_SIZE..MAX_SIZE) {
+            var max = start
             val sequence = StringBuilder()
             var value = start
             var iterations = 0
@@ -23,13 +26,14 @@ class TestCollatz {
                     value /= 2
                 } else {
                     value = value * 3 + 1
+                    if (value > max) max = value
                 }
           if (DISPLAY_SEQUENCE) {
               sequence.append('→')
               sequence.append(value)
           }
         } while ((value != 1L))
-            println("$start X $iterations ${sequence}")
+            println("$start X $iterations max: $max ${sequence}")
         }
     }
 }
