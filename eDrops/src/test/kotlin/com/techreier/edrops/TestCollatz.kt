dc76a0d500d1889584205d3f22c375373f68bdc0
Hans Reier Sigmond <reier.sigmond@gmail.com>
2024-03-05 19:35:56 +0100

    forbedret output

--- eDrops/src/test/kotlin/com/techreier/edrops/TestCollatz.kt
+++ eDrops/src/test/kotlin/com/techreier/edrops/TestCollatz.kt
@@ -3,20 +3,20 @@ package com.techreier.edrops
 import org.junit.jupiter.api.Test
 
 const val MIN_SIZE = 1L
-const val MAX_SIZE = 30L
+const val MAX_SIZE = 300L
 const val DISPLAY_SEQUENCE = true
 
 //Collatz Conjecture
 //Collatz formodning
 //https://www.quantamagazine.org/why-mathematicians-still-cant-solve-the-collatz-conjecture-20200922/
 //divide by two if result is integer else multiply by 3 and add one. Repeat.
-//You're out if the number is 1 or more than 100
+// 8 minutes. As many numbers as possible until you reach 1
+// If your not down to one after 8 minutes you loose.
 class TestCollatz {
 
     @Test
     fun testCollatz() {
         for (start in MIN_SIZE..MAX_SIZE) {
-            var max = start
             val sequence = StringBuilder()
             var value = start
             var iterations = 0
@@ -26,14 +26,13 @@ class TestCollatz {
                     value /= 2
                 } else {
                     value = value * 3 + 1
-                    if (value > max) max = value
                 }
           if (DISPLAY_SEQUENCE) {
               sequence.append('→')
               sequence.append(value)
           }
         } while ((value != 1L))
-            println("$start X $iterations max: $max ${sequence}")
+            println("$start X $iterations ${sequence}")
         }
     }
 }
