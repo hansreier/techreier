c65e4d07693fa7a9da16004cca2e583e4e48d739
Hans Reier Sigmond <reier.sigmond@gmail.com>
2024-07-24 18:08:01 +0200

    Omskrevet slik at det blir lagret med full presisjon

--- eDrops/src/main/kotlin/com/techreier/edrops/util/Memory.kt
+++ eDrops/src/main/kotlin/com/techreier/edrops/util/Memory.kt
@@ -1,41 +1,42 @@
 package com.techreier.edrops.util
 
-import org.slf4j.LoggerFactory
 import java.lang.management.ManagementFactory
 import java.lang.management.MemoryMXBean
 import java.lang.management.ThreadMXBean
 import java.math.BigDecimal
 import java.math.RoundingMode
 
-const val MByte = 1024 * 1024
-
-private val logger = LoggerFactory.getLogger("memory")
+const val MBYTE = 1024 * 1024
 
 fun mem(): MB {
     val memory: MemoryMXBean = ManagementFactory.getMemoryMXBean()
     val os = ManagementFactory.getOperatingSystemMXBean() as? com.sun.management.OperatingSystemMXBean
-    val cpuLoad:  BigDecimal? = os?.let { BigDecimal(it.cpuLoad).setScale(3, RoundingMode.HALF_UP) }
     val threads: ThreadMXBean = ManagementFactory.getThreadMXBean()
     val mem = MB(
-        memory.heapMemoryUsage.init / MByte,
-        memory.heapMemoryUsage.used / MByte,
-        memory.heapMemoryUsage.committed / MByte,
-        memory.heapMemoryUsage.max / MByte,
+        memory.heapMemoryUsage.init,
+        memory.heapMemoryUsage.used,
+        memory.heapMemoryUsage.committed,
+        memory.heapMemoryUsage.max,
         threads.threadCount,
         Thread.currentThread().isVirtual,
-        cpuLoad
+        os?.cpuLoad
     )
     return mem
 }
 
-fun logMem() {
-    logger.info("${mem()}")
-}
-
 data class MB(
     val init: Long, val used: Long, val committed: Long, val max: Long, val threads: Int,
-    val virtual: Boolean, val cpu: BigDecimal?
-)
+    val virtual: Boolean, val cpu: Double?
+) {
+    override fun toString(): String {
+        return "init=${init / MBYTE}MB, used=${used / MBYTE}MB, committed=${committed / MBYTE}MB" +
+                ", max=${max / MBYTE}MB, ${if (virtual) "vthreads=" else "threads="}$threads, cpu=${cpuPercent(cpu)}%"
+    }
+
+    private fun cpuPercent(cpuLoad: Double?): BigDecimal? {
+        return cpuLoad?.let { BigDecimal(cpuLoad * 100).setScale(2, RoundingMode.HALF_UP) }
+    }
+}
 
 
 
