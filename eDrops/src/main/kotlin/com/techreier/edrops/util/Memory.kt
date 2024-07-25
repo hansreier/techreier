3fcaf78e2c65943e25a76645eefc14b9461123cb
Hans Reier Sigmond <reier.sigmond@gmail.com>
2024-07-25 11:40:29 +0200

    Fjernet logging av cpu fra memory, for upålitelig uansett i containere.

--- eDrops/src/main/kotlin/com/techreier/edrops/util/Memory.kt
+++ eDrops/src/main/kotlin/com/techreier/edrops/util/Memory.kt
@@ -3,14 +3,10 @@ package com.techreier.edrops.util
 import java.lang.management.ManagementFactory
 import java.lang.management.MemoryMXBean
 import java.lang.management.ThreadMXBean
-import java.math.BigDecimal
-import java.math.RoundingMode
 
 const val MBYTE = 1024 * 1024
-
 fun mem(): MB {
     val memory: MemoryMXBean = ManagementFactory.getMemoryMXBean()
-    val os = ManagementFactory.getOperatingSystemMXBean() as? com.sun.management.OperatingSystemMXBean
     val threads: ThreadMXBean = ManagementFactory.getThreadMXBean()
     val mem = MB(
         memory.heapMemoryUsage.init,
@@ -18,23 +14,18 @@ fun mem(): MB {
         memory.heapMemoryUsage.committed,
         memory.heapMemoryUsage.max,
         threads.threadCount,
-        Thread.currentThread().isVirtual,
-        os?.cpuLoad
+        Thread.currentThread().isVirtual
     )
     return mem
 }
 
 data class MB(
     val init: Long, val used: Long, val committed: Long, val max: Long, val threads: Int,
-    val virtual: Boolean, val cpu: Double?
+    val virtual: Boolean
 ) {
     override fun toString(): String {
         return "init=${init / MBYTE}MB, used=${used / MBYTE}MB, committed=${committed / MBYTE}MB" +
-                ", max=${max / MBYTE}MB, ${if (virtual) "vthreads=" else "threads="}$threads, cpu=${cpuPercent(cpu)}%"
-    }
-
-    private fun cpuPercent(cpuLoad: Double?): BigDecimal? {
-        return cpuLoad?.let { BigDecimal(cpuLoad * 100).setScale(2, RoundingMode.HALF_UP) }
+                ", max=${max / MBYTE}MB, ${if (virtual) "vthreads=" else "threads="}$threads"
     }
 }
 
