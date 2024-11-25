15c36fe961e21cb40d00db9d494444ee1ec31b39
Hans Reier Sigmond <reier.sigmond@gmail.com>
2024-11-25 14:58:14 +0100

    Fikset alle repo testene

---
+++ eDrops/src/main/resources/db/migration/V1__Initial.sql
@@ -0,0 +1,78 @@
+CREATE TABLE blog
+(
+    blog_owner_id BIGINT       NOT NULL,
+    changed       TIMESTAMP(0) NOT NULL,
+    id            BIGINT       NOT NULL AUTO_INCREMENT,
+    language_code VARCHAR(15)  NOT NULL,
+    segment       VARCHAR(30)  NOT NULL,
+    subject       VARCHAR(50)  NOT NULL,
+    about         VARCHAR(400) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+CREATE TABLE blog_entry
+(
+    blog_id BIGINT       NOT NULL,
+    changed TIMESTAMP(0) NULL,
+    id      BIGINT       NOT NULL AUTO_INCREMENT,
+    segment VARCHAR(30)  NOT NULL,
+    title   VARCHAR(50)  NOT NULL,
+    summary VARCHAR(400) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+CREATE TABLE blog_owner
+(
+    changed      TIMESTAMP(0) NULL,
+    created      TIMESTAMP(0) NOT NULL,
+    id           BIGINT       NOT NULL AUTO_INCREMENT,
+    country_code VARCHAR(15)  NOT NULL,
+    zip_code     VARCHAR(15)  NOT NULL,
+    username     VARCHAR(60)  NOT NULL,
+    address      VARCHAR(255) NOT NULL,
+    e_mail       VARCHAR(255) NOT NULL,
+    first_name   VARCHAR(255) NOT NULL,
+    last_name    VARCHAR(255) NOT NULL,
+    location     VARCHAR(255) NOT NULL,
+    password     VARCHAR(255),
+    phones       VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+CREATE TABLE blog_text
+(
+    blog_entry_id BIGINT       NOT NULL,
+    changed       TIMESTAMP(0) NULL,
+    text          TEXT         NOT NULL,
+    PRIMARY KEY (blog_entry_id)
+) ENGINE = InnoDB;
+
+CREATE TABLE language_code
+(
+    code     VARCHAR(15)  NOT NULL,
+    language VARCHAR(255) NOT NULL,
+    PRIMARY KEY (code)
+) ENGINE = InnoDB;
+
+ALTER TABLE blog_owner
+    ADD CONSTRAINT uk_blog_owner_username UNIQUE (username);
+
+ALTER TABLE blog
+    ADD CONSTRAINT fk_blog_blog_owner
+        FOREIGN KEY (blog_owner_id)
+            REFERENCES blog_owner (id);
+
+ALTER TABLE blog
+    ADD CONSTRAINT fk_blog_language_code
+        FOREIGN KEY (language_code)
+            REFERENCES language_code (code);
+
+ALTER TABLE blog_entry
+    ADD CONSTRAINT fk_blog_entry_blog
+        FOREIGN KEY (blog_id)
+            REFERENCES blog (id);
+
+ALTER TABLE blog_text
+    ADD CONSTRAINT fk_blog_text_blog_entry
+        FOREIGN KEY (blog_entry_id)
+            REFERENCES blog_entry (id);
