192254a4be8cb6edd86b7f6899baad8c32c092d7
Hans Reier Sigmond <reier.sigmond@gmail.com>
2025-03-08 21:22:50 +0100

    nå virker det nesten

--- eDrops/src/main/resources/static/js/dropdown.js
+++ eDrops/src/main/resources/static/js/dropdown.js
@@ -1,13 +1,18 @@
+
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll(".dropdown-item").forEach(function (item) {
+
         if (item.dataset.collapsible === "true") {
             item.addEventListener("click", function (event) {
                 event.preventDefault(); // Prevent navigation
+                item.classList.toggle("expandable")
+                item.classList.add("collapsible")
                 let nextElement = item.nextElementSibling;
                 while (nextElement &&
                         nextElement.classList.contains("dropdown-item") &&
                         nextElement.dataset.collapsible === "false")  {
                     nextElement.classList.toggle("collapsed");
+                    nextElement.classList.add("collapsible");
                     nextElement = nextElement.nextElementSibling;
                 }
             });
