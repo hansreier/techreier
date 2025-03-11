acf966106a70a012441889e72eda3da70e439325
Hans Reier Sigmond <hansreier.sigmond@skatteetaten.no>
2025-03-11 22:41:07 +0100

    fikset slik at flere menyer virker

--- eDrops/src/main/resources/static/js/dropdown.js
+++ eDrops/src/main/resources/static/js/dropdown.js
@@ -1,55 +1,57 @@
-
 document.addEventListener("DOMContentLoaded", function () {
 
     // Since the dropdown menus are dynamically generated, have to add class for css selectors to work.
-    let top = true
-    document.querySelectorAll(".dropdown-item").forEach(function (item) {
-        if (item.dataset.topic === "true") {
-            top = false
-        }
-        if (!top) {
-            item.classList.add("topic")
-            let expandable = sessionStorage.getItem(item.dataset.id + ".expandable")
-            let collapsed = sessionStorage.getItem(item.dataset.id + ".collapsed")
-            console.log("id", item.dataset.id,
-                        "collapsed: ", sessionStorage.getItem(item.dataset.id + ".collapsed"),
-                        "expandable: ", sessionStorage.getItem(item.dataset.id + ".expandable"))
+    document.querySelectorAll(".dropdown").forEach(function (menu) {
+        let top = true
+        menu.querySelectorAll(".dropdown-item").forEach(function (item) {
+            console.log("top")
             if (item.dataset.topic === "true") {
-
-                if (expandable === "true" || (expandable == null)) {
-                    item.classList.add("expandable")
-                } else {
-                    item.classList.remove("expandable")
-                }
-            } else {
-                if (collapsed === "true" || (collapsed == null)) {
-                    item.classList.add("collapsed")
-                } else {
-                    item.classList.remove("collapsed")
-                }
+                top = false
             }
-        }
-    });
+            if (!top) {
+                item.classList.add("topic")
+                let expandable = sessionStorage.getItem(item.dataset.id + ".expandable")
+                let collapsed = sessionStorage.getItem(item.dataset.id + ".collapsed")
+                console.log("id", item.dataset.id,
+                    "collapsed: ", sessionStorage.getItem(item.dataset.id + ".collapsed"),
+                    "expandable: ", sessionStorage.getItem(item.dataset.id + ".expandable"))
+                if (item.dataset.topic === "true") {
 
-    //listen to clicks on dropdown menu
-    document.querySelectorAll(".dropdown-item[data-topic='true']")
-        .forEach(function (item) {
-        item.addEventListener("click", function (event) {
-            event.preventDefault(); // Prevent navigation
-            let expandable = item.classList.toggle("expandable")
-            console.log("saving expandable in session storage", item.dataset.id, "expandable", expandable)
-            sessionStorage.setItem(item.dataset.id + ".expandable", String(expandable))
-            let nextElement = item.nextElementSibling;
-            while (nextElement &&
-            nextElement.classList.contains("dropdown-item") &&
-            nextElement.dataset.topic === "false") {
-                let collapsed = nextElement.classList.toggle("collapsed");
-                nextElement.classList.add("topic");
-                console.log("saving collapsed in session storage", nextElement.dataset.id, "collapsed", collapsed)
-                sessionStorage.setItem(nextElement.dataset.id + ".collapsed", String(collapsed));
-                nextElement = nextElement.nextElementSibling;
+                    if (expandable === "true" || (expandable == null)) {
+                        item.classList.add("expandable")
+                    } else {
+                        item.classList.remove("expandable")
+                    }
+                } else {
+                    if (collapsed === "true" || (collapsed == null)) {
+                        item.classList.add("collapsed")
+                    } else {
+                        item.classList.remove("collapsed")
+                    }
+                }
             }
         });
-
     });
+
+    // listen to clicks on dropdown menu
+    document.querySelectorAll(".dropdown-item[data-topic='true']")
+        .forEach(function (item) {
+            item.addEventListener("click", function (event) {
+                event.preventDefault(); // Prevent navigation
+                let expandable = item.classList.toggle("expandable")
+                console.log("saving expandable in session storage", item.dataset.id, "expandable", expandable)
+                sessionStorage.setItem(item.dataset.id + ".expandable", String(expandable))
+                let nextElement = item.nextElementSibling;
+                while (nextElement &&
+                nextElement.classList.contains("dropdown-item") &&
+                nextElement.dataset.topic === "false") {
+                    let collapsed = nextElement.classList.toggle("collapsed");
+                    nextElement.classList.add("topic");
+                    console.log("saving collapsed in session storage", nextElement.dataset.id, "collapsed", collapsed)
+                    sessionStorage.setItem(nextElement.dataset.id + ".collapsed", String(collapsed));
+                    nextElement = nextElement.nextElementSibling;
+                }
+            });
+
+        });
 });
