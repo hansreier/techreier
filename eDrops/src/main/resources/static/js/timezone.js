241619489278a92b4f400fc4c8ed04e58392a5d2
Hans Reier Sigmond <reier.sigmond@gmail.com>
2025-04-26 13:04:24 +0200

    lagt inn lesing av time zone

--- eDrops/src/main/resources/static/js/timezone.js
+++ eDrops/src/main/resources/static/js/timezone.js
@@ -1,7 +1,19 @@
 window.addEventListener('load', function() {
-    fetch('/timezone', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ timezone: Intl.DateTimeFormat().resolvedOptions().timeZone })
-    });
+    const storedTimeZone = sessionStorage.getItem('timezone');
+
+    if (!storedTimeZone) {
+        const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+        fetch('/timezone', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ timezone: timezone })
+        })
+            .then(response => {
+                sessionStorage.setItem('timezone', timezone);
+            })
+            .catch(error => {
+                console.error('Error setting timezone:', error);
+            });
+    }
 });
