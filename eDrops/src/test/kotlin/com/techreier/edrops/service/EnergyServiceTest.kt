0d3457c272c5d95807817a961b880ab141de3b31
Hans Reier Sigmond <reier.sigmond@gmail.com>
2025-09-20 18:42:49 +0200

    fikset test som måtte omskrives.

--- eDrops/src/test/kotlin/com/techreier/edrops/service/EnergyServiceTest.kt
+++ eDrops/src/test/kotlin/com/techreier/edrops/service/EnergyServiceTest.kt
@@ -1,33 +1,37 @@
 package com.techreier.edrops.service
 
+import com.techreier.edrops.model.EnergyValues
+import org.assertj.core.api.Assertions.fail
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 
 class EnergyServiceTest {
 
-    private val energyService = EnergyService()
+    private val service = EnergyService()
+    private val ep2024 = service.energyYears[2024]
 
-    /* TODO Rewrite
     @Test
     fun energyTest() {
-        assertEquals(54, energyService.energyProduction.size)
-        val ep2024 = energyService.energyProduction[2024]
+        assertEquals(54, service.energyYears.count())
         assertNotNull(ep2024)
-        assertEquals(139.984, ep2024!!.waterTWh!!, 0.001)
-        assertEquals(503.9424, ep2024.waterTJ!!, 0.001)
-        assertEquals(14.545, ep2024.windTWh!!, 0.001)
-        assertEquals(52.362, ep2024.windTJ!!, 0.001)
-        assertEquals(0.251, ep2024.solarTWh!!, 0.001)
-        assertEquals(0.9036, ep2024.solarTJ!!, 0.001)
-        assertEquals(2.357, ep2024.heatTWh!!, 0.001)
-        assertEquals(8.4852, ep2024.heatTJ!!, 0.001)
-        assertEquals(116.36, ep2024.oilMSm3!!, 0.001)
-        assertEquals(1657.3243, ep2024.oilTWh!!, 0.001)
-        assertEquals(16125.317879, ep2024.oilTJ!!, 0.001)
-        assertEquals(126.23753, ep2024.gasMSm3!!, 0.001)
-        assertEquals(2429.7444, ep2024.gasTWh!!, 0.001)
-        assertEquals(17494.160292, ep2024.gasTJ!!, 0.001)
-    }*/
+        assertEquals(139.984, value(EnergySource.WATER).twh ?: 0.0, 0.001)
+        assertEquals(503.9424, value(EnergySource.WATER).tj ?: 0.0, 0.001)
+        assertEquals(14.545, value(EnergySource.WIND).twh ?: 0.0, 0.001)
+        assertEquals(52.362, value(EnergySource.WIND).tj ?: 0.0, 0.001)
+        assertEquals(0.251, value(EnergySource.SOLAR).twh ?: 0.0, 0.001)
+        assertEquals(0.9036, value(EnergySource.SOLAR).tj ?: 0.0, 0.001)
+        assertEquals(2.357, value(EnergySource.HEAT).twh ?: 0.0, 0.001)
+        assertEquals(8.4852, value(EnergySource.HEAT).tj ?: 0.0, 0.001)
+        assertEquals(116.36, value(EnergySource.OIL).orig ?: 0.0, 0.001)
+        assertEquals(1657.3243, value(EnergySource.OIL).twh ?: 0.0, 0.001)
+        assertEquals(16125.317879, value(EnergySource.OIL).tj ?: 0.0, 0.001)
+        assertEquals(126.23753, value(EnergySource.GAS).orig ?: 0.0, 0.001)
+        assertEquals(2429.7444, value(EnergySource.GAS).twh ?: 0.0, 0.001)
+        assertEquals(17494.160292, value(EnergySource.GAS).tj ?: 0.0, 0.001)
+    }
+
+    private fun value(source: EnergySource): EnergyValues =
+        ep2024?.find { e -> e.source == source } ?: fail("Could not find source ${source}")
 
 }
