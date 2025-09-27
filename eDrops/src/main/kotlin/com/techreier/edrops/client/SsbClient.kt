4ae73445b6c394ca8e6a479cb93a2e88dcff2e6a
Hans Reier Sigmond <reier.sigmond@gmail.com>
2025-09-27 17:31:19 +0200

    lagt inn ssb test

--- eDrops/src/main/kotlin/com/techreier/edrops/client/SsbClient.kt
+++ eDrops/src/main/kotlin/com/techreier/edrops/client/SsbClient.kt
@@ -3,37 +3,45 @@ package com.techreier.edrops.client
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestClient
+// https://www.ssb.no/statbank/table/11561
+// https://www.ssb.no/energi-og-industri/energi/statistikk/produksjon-og-forbruk-av-energi-energibalanse-og-energiregnskap
 
 @Component
 class SsbClient(private val restClient: RestClient) {
 
     fun fetchEnergyData(): String {
         val jsonQuery = """
-        {
-          "query": [
-            {
-              "code": "Energibalanse",
-              "selection": {
-                "filter": "vs:Energibalansepost01",
-                "values": [
-                  "EB06"
-                ]
-              }
-            },
-            {
-              "code": "EnergiProdukt",
-              "selection": {
-                "filter": "vs:EnergiProdukt",
-                "values": [
-                  "EP04IF"
-                ]
-              }
-            }
-          ],
-          "response": {
-            "format": "json-stat2"
-          }
-        }
+       {
+  "query": [
+    {
+      "code": "Energibalanse",
+      "selection": {
+        "filter": "vs:Energibalansepost01",
+        "values": [
+          "EB01",
+          "EB02",
+          "EB03",
+          "EB06"
+        ]
+      }
+    },
+    {
+      "code": "EnergiProdukt",
+      "selection": {
+        "filter": "vs:EnergiProdukt",
+        "values": [
+          "EP03",
+          "EP04IF",
+          "EP07",
+          "EP081_84"
+        ]
+      }
+    }
+  ],
+  "response": {
+    "format": "json-stat2"
+  }
+}
         """.trimIndent()
 
         return restClient.post()
