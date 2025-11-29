46d1c697813517bfe859a024bd77407a9c34d64e
Hans Reier Sigmond <hansreier.sigmond@skatteetaten.no>
2025-11-29 14:48:25 +0100

    Innført Spring Boot 4

--- eDrops/src/test/kotlin/com/techreier/edrops/SpringContextTest.kt
+++ eDrops/src/test/kotlin/com/techreier/edrops/SpringContextTest.kt
@@ -4,7 +4,6 @@ import com.techreier.edrops.config.logger
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @ExtendWith(SpringExtension::class)
