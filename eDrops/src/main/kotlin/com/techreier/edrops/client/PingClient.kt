af8e4c0babf6744b5189206b10c65a7b1a2e83b3
Hans Reier Sigmond <hansreier.sigmond@skatteetaten.no>
2026-02-03 11:49:01 +0100

    Nå funker det for Spring Boot 4

--- eDrops/src/main/kotlin/com/techreier/edrops/client/PingClient.kt
+++ eDrops/src/main/kotlin/com/techreier/edrops/client/PingClient.kt
@@ -2,13 +2,14 @@ package com.techreier.edrops.client
 
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestClient
+import org.springframework.web.client.body
 
 @Component
 class PingClient(builder: RestClient.Builder) {
     private val restClient = builder.build()
 
     fun ping(): String? =
-        restClient.get().uri("/api/ping").retrieve().body(String::class.java)
+        restClient.get().uri("/api/ping").retrieve().body<String>()
 
     fun ping(text: String): String? =
         restClient.get().uri { it.path("/api/ping").queryParam("input", text).build() }
