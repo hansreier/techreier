5ce98ab11e765579533c141078ea9aa7ea6d042e
Hans Reier Sigmond <hansreier.sigmond@skatteetaten.no>
2026-03-20 18:23:45 +0100

    forenklet languageController

--- eDrops/src/main/kotlin/com/techreier/edrops/controllers/LanguageController.kt
+++ eDrops/src/main/kotlin/com/techreier/edrops/controllers/LanguageController.kt
@@ -3,20 +3,17 @@ package com.techreier.edrops.controllers
 import com.techreier.edrops.config.logger
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.servlet.mvc.support.RedirectAttributes
 
 @Controller
 @RequestMapping
 class LanguageController {
     @PostMapping("/language")
     fun getLanguage(
-        redirectAttributes: RedirectAttributes,
         languageCode: String?,
-        blogId: Long?,
         path: String,
     ): String {
         logger.info("POST /language, and redirect")
-        logger.debug("Language selected: {} path: {} blogId: {}", languageCode, path, blogId)
+        logger.debug("Language selected: {} path: {}", languageCode, path)
         logger.debug("before redirect to get")
         return "redirect:$path?lang=$languageCode"
     }
